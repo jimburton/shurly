1d1d3033c9457b6340fca8135eacbd977fd37611
Jim Burton <j.burton@brighton.ac.uk>
2018-04-01 15:03:58 +0100

    Started using typesafe config lib

--- src/main/java/CI346/shurly/Shurly.java
+++ src/main/java/CI346/shurly/Shurly.java
@@ -1,6 +1,8 @@
 package CI346.shurly;
 
 import com.google.common.hash.Hashing;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
@@ -20,20 +22,32 @@ public class Shurly {
     private static Model model = null;
 
     public static void main(String[] args) {
-        // Configure Spark
-        port(4567);
-        staticFiles.location("/public");
-        staticFiles.expireTime(600L);
+        //Read in the config
+        Config conf         = ConfigFactory.load();
+        int port            = conf.getInt("web.port");
+        String host         = conf.getString("web.host");
+        String HOME         = host +":"+port;
+        String staticLoc    = conf.getString("web.staticFiles");
+        String templatePath = conf.getString("web.templatePath");
+        String indexPath    = templatePath + "index.vm";
+        long staticTO       = conf.getLong("web.staticTimeout");
+        String dbConnString = conf.getString("db.connectionString");
+        String dbUser       = conf.getString("db.user");
+        String dbPass       = conf.getString("db.password");
 
-        Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/shurly",
-                "shurly", "shurly");
+        // Configure Spark
+        port(port);
+        staticFiles.location(staticLoc);
+        staticFiles.expireTime(staticTO);
+
+        Sql2o sql2o = new Sql2o(dbConnString, dbUser, dbPass);
         model = new Sql2oModel(sql2o);
 
         post("/", (req, res) -> {
                     String theURL = req.queryParams("the_url");
                     log.info("received POST: " + theURL);
                     final Map<String, Object> pageModel = new HashMap<>();
-                    pageModel.put("HOME", "http://localhost:4567");
+                    pageModel.put("HOME", HOME);
                     if (!isValidURL(theURL)) {
                         pageModel.put("ERROR", "INVALID_URL");
                     } else {
@@ -46,12 +60,12 @@ public class Shurly {
                         pageModel.put("URL", theURL);
                         pageModel.put("ENC", enc);
                     }
-                    return render(pageModel, "templates/index.vm");
+                    return render(pageModel, indexPath);
                 });
 
         get("/", (req, res) -> {
             log.info("received GET");
-            return render(new HashMap<>(), "templates/index.vm");
+            return render(new HashMap<>(), indexPath);
         });
 
         get("/:enc", (req, res) -> {
@@ -59,7 +73,7 @@ public class Shurly {
             if(theURL.equals(Sql2oModel.URL_NOT_FOUND)) {
                 final Map<String, Object> pageModel = new HashMap<>();
                 pageModel.put("ERROR", "URL_NOT_FOUND");
-                return render(pageModel, "templates/index.vm");
+                return render(pageModel, indexPath);
             } else {
                 res.redirect(theURL);
             }
